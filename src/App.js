b3a7bfdd2d90f67cd8be6de98865325b404320f0
Bekzhan-code <aldashov.bekzhan@gmail.com>
2024-09-16 17:44:26 +0500

    made loading skeleton to pizza card component, added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
     <div className="bg-customYellow py-11">
       <div className="bg-white w-11/12 mx-auto rounded-xl px-16">
         <Header />
-        <HomePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,14 +2,19 @@ import React, { useEffect, useState } from "react";
 import Categories from "../components/Categories";
 import SortPopup from "../components/SortPopup";
 import PizzaCard from "../components/PizzaCard";
+import { Skeleton } from "../components/PizzaCard/Skeleton";
 
 const Home = () => {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     fetch("https://eaed36219e51a8b4.mokky.dev/items")
       .then((res) => res.json())
-      .then((arr) => setItems(arr));
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false)
+      });
   }, []);
 
   return (
@@ -22,9 +27,7 @@ const Home = () => {
       <h1 className="text-3xl font-bold mb-9 md:hidden">Все пиццы</h1>
 
       <div className="grid place-items-center grid-cols-4 gap-y-9 pb-24 2xl:grid-cols-3 xl:grid-cols-2 md:grid-cols-1">
-        {items.map((pizza) => (
-          <PizzaCard key={pizza.id} {...pizza} />
-        ))}
+        {isLoading ? [...new Array(8)].map((_, index) => (<Skeleton key={index} />))  : items.map(pizza => (<PizzaCard key={pizza.id} {...pizza} /> ))}
       </div>
     </div>
   );
